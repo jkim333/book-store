7bb38c312635b025cf9c767d73bc1a4174705ba5
Jihyung Kim <kimjihyung3@gmail.com>
2021-03-22 20:52:20 +1100

    Set up custom user model
    The model takes in email, username and password to create a user.
    Requires email and password to login.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+CustomUser = get_user_model()
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = (
+        (None, {'fields': (
+            'email',
+            'username',
+            'password',
+            'last_login'
+        )}),
+        ('Additional Info', {'fields': (
+            ('first_name', 'last_name'),
+        )}),
+        ('Permissions', {'fields': (
+            'is_active',
+            'is_superuser',
+            'groups',
+            'user_permissions',
+        )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'fields': ('email', 'username', 'password1', 'password2')
+        }),
+    )
+    list_display = ['email', 'username',]
+
